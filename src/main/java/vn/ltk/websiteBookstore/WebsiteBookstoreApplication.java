ec30975587bfbb5276753931f004b98d8e732641
imkhari <letuankhai.hk@gmail.com>
2025-01-21 20:59:18 +0700

    Create a new project

---
+++ src/main/java/vn/ltk/websiteBookstore/WebsiteBookstoreApplication.java
@@ -0,0 +1,13 @@
+package vn.ltk.websiteBookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsiteBookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsiteBookstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/ltk/websiteBookstore/WebsiteBookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.ltk.websiteBookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsiteBookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
